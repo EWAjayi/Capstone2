5dfe4dfc57720cb8ed94b56af6828240897aa8c6
EWAjayi <e.ajayi97@gmail.com>
2023-11-15 10:07:44 -0800

    need to debug sortByParkType

--- scripts/index.js
+++ scripts/index.js
@@ -1,6 +1,7 @@
 "use strict"
 
 window.onload = () => {
+
     // location to output display
     const states = document.getElementById("states");
     const parkType = document.getElementById("parkType");
@@ -9,12 +10,11 @@ window.onload = () => {
     // addParkTypesToDropdown();
 };
 
-//TODO create first layer: Location dropdown and display onchange
-function addLocationsToDropdown() {
-    // Get the dropdown element by its ID
-    
+//TODO create first layer: Add informatin to dropdown menus
+    function addLocationsToDropdown() {
         // Use forEach to iterate over the array
         locationsArray.forEach(function (location) {
+            
             // Create a new option element
             let option = document.createElement("option");
             
@@ -22,50 +22,12 @@ function addLocationsToDropdown() {
             option.value = location;
             option.textContent = location;
             
-            // Add an event listener to the option
-            //*Add event listener to the dropdown options
-            // option.addEventListener("click", displayLocationInfo(location));
-            
             // Add the new option to the dropdown
             states.appendChild(option);
         });
     }
-    
-    
-//TODO create function for display after onclick
-    //*Display information based on the state chosen
-    function displayLocationInfo() {
-        let stateValue = document.getElementById("states").value
-        console.log(stateValue);
-        // Filter the nationalParksArray based on the selectedLocation
-        const filteredParks = nationalParksArray.filter(function (park) {
-            return park.State === stateValue;
-        });
-        
-        // Display the filtered information
-        let message = `${filteredParks.length} National Parks in ${stateValue}</h1><br><br>`;
-        message += filteredParks.map(parkTemplate).join("");
-        addParkTypesToDropdown();
-        document.getElementById("myParks").innerHTML = message;
-    }
-    
-    //* Template for display output
-    function parkTemplate(park) {
-        return `
-            <div class="card" style="width: 18rem;">
-                <img src="${park.Image}" class="card-img-top" alt="...">
-                <div class="card-body">
-                    <h5 class="card-title">${park.LocationName}</h5>
-                    <p class="card-text">${park.State}</p>
-                    <a href="#" class="btn btn-primary">Go somewhere</a>
-                </div>
-            </div>`;
-    }
-    
-    //TODO create second layer: filter for park type of the displayed information onchange
+
     function addParkTypesToDropdown(){
-        // Get the dropdown element by its ID
-        
         // Use forEach to iterate over the array
         parkTypesArray.forEach(function (types) {
             // Create a new option element
@@ -78,11 +40,88 @@ function addLocationsToDropdown() {
             // Add the new option to the dropdown
             parkType.appendChild(option);
         });
-        
-        //*Display information based on the type of park chosen
-        
-        //*Add event listener to change display with chosen type
     }
+
+//TODO create function for display after onclick
+    //*Display information based on the state chosen
+        function displayLocationInfo() {
+            let stateValue = document.getElementById("states").value
+            console.log(stateValue);
+            // Filter the nationalParksArray based on the selectedLocation
+            const filteredParks = nationalParksArray.filter(function (park) {
+                return park.State === stateValue;
+            });
+            
+            //Function to display additional options
+            function showTypeOptions() {
+                // Get the second button element
+                let parkType = document.getElementById("parkType");
+                let parkTypeButton = document.getElementById("parkTypeButton");
+                // Change the display style to block (show the button)
+                parkType.style.display = "inline";
+                parkTypeButton.style.display = "inline";
+            }
+
+            // Display the filtered information
+            let message = `${filteredParks.length} National Parks in ${stateValue}</h1><br><br>`;
+            message += filteredParks.map(parkTemplate).join("");
+            addParkTypesToDropdown();
+            showTypeOptions();
+            document.getElementById("myParks").innerHTML = message;
+
+            // Allow list to be used in other functions
+            return filteredParks;
+        }
+
+    //* Template for location display output
+        function parkTemplate(park) {
+            return `
+                <div class="card" style="width: 18rem;">
+                    <img src="${park.Image}" class="card-img-top" alt="...">
+                    <div class="card-body">
+                        <h5 class="card-title">${park.LocationName}</h5>
+                        <p class="card-text">${park.State}</p>
+                        <a href="#" class="btn btn-primary">Go somewhere</a>
+                    </div>
+                </div>`;
+        }
+    
+//TODO create second layer: filter park types
+    //Display information based on the park type chosen
+    function sortByParkType() {
+
+        let typeValue = document.getElementById("parkType").value
+        console.log(parkType);
+
+        // Filter the parkType based on pulling from Name
+        function filterParkNames (typeValue) {
+            let filteredType = filteredParks.filter(function (park) {
+                return park.LocationName
+            });
+
+            return filteredType;
+        }
+
+        // Display the filtered information
+        let message = `There are ${filteredType.length} ${parkType}s in ${stateValue}</h1><br><br>`;
+        message += filteredType.map(filteredParkTemplate).join("");
+        // addParkTypesToDropdown();
+        // showTypeOptions();
+        document.getElementById("myParks").innerHTML = message;
+    }    
+
+    //* Template for displaying filtered output
+    function filteredParkTemplate(park) {
+        return `
+            <div class="card" style="width: 18rem;">
+                <img src="${park.Image}" class="card-img-top" alt="...">
+                <div class="card-body">
+                    <h5 class="card-title">${park.LocationName}</h5>
+                    <p class="card-text">${park.State}</p>
+                    <a href="#" class="btn btn-primary">Go somewhere</a>
+                </div>
+            </div>`;
+    }    
     
     
     
